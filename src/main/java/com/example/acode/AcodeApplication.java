bc0719545a0fdb5b7ae5c6f1572d16f09780af6b
Karina Cardoso Telles <karina@divibank.co>
2021-03-30 16:51:17 -0300

    Adding enf-points

--- src/main/java/com/example/acode/AcodeApplication.java
+++ src/main/java/com/example/acode/AcodeApplication.java
@@ -2,12 +2,26 @@ package com.example.acode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class AcodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AcodeApplication.class, args);
+    }
+    /*root*/
+    @GetMapping
+    public String hello(){
+        return "Hello World";
+    }
+    @GetMapping ("/json")
+    public List<String> hello2(){
+        return List.of("Hello!", "World");
     }
 
 }
