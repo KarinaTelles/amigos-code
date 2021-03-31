6ccda2555f965b928c0482753d568fa2a1fb2858
Karina Cardoso Telles <karina@divibank.co>
2021-03-31 16:45:01 -0300

    Changing controller, adding Student Controller

--- src/main/java/com/example/acode/AcodeApplication.java
+++ src/main/java/com/example/acode/AcodeApplication.java
@@ -11,26 +11,9 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class AcodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AcodeApplication.class, args);
     }
-    /*root*/
-    @GetMapping
-    public String hello(){
-        return "Hello World";
-    }
-    @GetMapping ("/json")
-    public List<String> hello2(){
-        return List.of("Hello!", "World");
-    }
-    @GetMapping("/student")
-    public List<Student> hello3(){
-        return List.of(new
-                Student(1L,"Maria",  LocalDate.of(1991, Month.FEBRUARY,22), 30,"Maria@email.com")
-        );
-    }
-
 }
